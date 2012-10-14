c71fd5838b4d7241d13000b30d46cdd5b9ea7f99
cbilling <cbilling@localhost>
2012-10-14 12:16:22 +0000

    Added qtutils; a bunch of utilities for facilitating multithreaded programming in qt. They are: qtlock: a context manager which ensures the mainloop is blocked whilst it is held. inmain(fn,*args,**kwargs): runs a function in the mainloop, waiting on it and returning its result. inmain_later(fn,*args,**kwargs): ask for a function to be run in the mainloop, not waiting on it. inmain_decorator(wait=True): a decorator which causes decorated functions to be executed in the mainloop (and either waited on on to return or not).
    Qt and Python are very hackable, and thanks to this the above utilities have functionality built in to protect from their misuse. Any deadlocks arising from their misuse ought to be raised as an exception, and any Qt method calls that aren't done in a threadsafe way ought to raise an exception too. Further, Qt normally spouts (contextless, impossible to see the origin of) warnings when threads are used incorrectly, these have been promoted to exceptions with full tracebacks.

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from PySide.QtCore import qInstallMsgHandler 
+
+def _message_handler(type, message):
+    """Handle qt warnings etc with an exception, so they don't pass
+    unnoticed"""
+    raise Exception('%s: %s'%(type,message))
+        
+qInstallMsgHandler(_message_handler)
+del qInstallMsgHandler
+
+from locking import qtlock
+qtlock.enforce()
+
+from invoke_in_main import inmain, inmain_later, inmain_decorator
+
+
+
---
+++ invoke_in_main.py
@@ -0,0 +1,77 @@
+import Queue
+import threading
+import functools
+import inspect
+
+from PySide.QtCore import *
+
+from locking import qtlock
+
+class CallEvent(QEvent):
+    """An event containing a request for a function call."""
+    EVENT_TYPE = QEvent.Type(QEvent.registerEventType())
+    def __init__(self, fn, *args, **kwargs):
+        QEvent.__init__(self, self.EVENT_TYPE)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.done = False
+        self._returnval = Queue.Queue()
+        self._lock = threading.Lock()
+        self.cancelled = False
+        
+    def wait(self):
+        if qtlock.held():
+            message = ('Deadlock: Either you are already in the main thread or you have acquired the qtlock. ' +
+                       'Either way the qt mainloop is blocked and the function you are waiting on will never run')
+            raise threading.ThreadError(message)
+        return self._returnval.get()
+    
+    def cancel(self):
+        with self._lock:
+            if self.done:
+                return False
+            self.cancelled = True
+            return True
+                
+                
+class Caller(QObject):
+    """An event handler which calls the function held within a CallEvent."""
+    def event(self, event):
+        with event._lock:
+            if not event.cancelled:
+                result = event.fn(*event.args, **event.kwargs)
+                event._returnval.put(result)
+                event.done = True
+        return True
+        
+caller = Caller()
+
+
+def inmain(fn, *args, **kwargs):
+    """Execute a function in the main thread. Wait for it to complete
+    and return its return value."""
+    if threading.current_thread().name == 'MainThread':
+        return fn(*args, **kwargs)
+    event = CallEvent(fn, *args, **kwargs)
+    QCoreApplication.postEvent(caller, event)
+    return event.wait()
+    
+def inmain_later(fn, *args, **kwargs):
+    """Asks the mainloop to call a function when it has time. Immediately
+    returns the event that was sent to the mainloop.  Functions are
+    guaranteed to be called in the order they were requested."""
+    event = CallEvent(fn, *args, **kwargs)
+    QCoreApplication.postEvent(caller, event)
+    return event
+        
+def inmain_decorator(fn, wait=True):
+    """A decorator which sets any function to always run in the main thread."""
+    @functools.wraps(fn)
+    def f(*args,**kwargs):
+        if wait:
+            return inmain(fn, *args, **kwargs)
+        return inmain_later(fn, *args, **kwargs)  
+    return f
+    
+
