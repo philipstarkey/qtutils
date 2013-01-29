c246d8f17dedc12d67bfc6607e85c2414525582e
Phil Starkey <philipstarkey@users.noreply.github.com>
2013-01-29 13:42:20 +1100

    Fixed the bug where clicking a combobox (that has an associated model) would freeze the program. It turns out that Bilbo had written a message handler to promote Qt Warnings to exceptions (in qtutils/__init__.py). Clicking a combobox with a model throws a Qt Warning "QAccessibleWidget::rect: This implementation does not support subelements! (ID 1 unknown for QWidget)" which we might want to fix later. The message handler raised an exception, which never showed and locked up the application. Apparently you can't raise exceptions there (maybe it is a pyside bug, who knows!)

--- __init__.py
+++ __init__.py
@@ -3,7 +3,8 @@ from PySide.QtCore import qInstallMsgHandler
 def _message_handler(type, message):
     """Handle qt warnings etc with an exception, so they don't pass
     unnoticed"""
-    raise Exception('%s: %s'%(type,message))
+    print '%s: %s'%(type,message)
+    #raise Exception('%s: %s'%(type,message))
         
 qInstallMsgHandler(_message_handler)
 del qInstallMsgHandler
