3ae4919c3cef4a521f7b2ed42140528f43409392
Chris Billington <chrisjbillington@gmail.com>
2013-09-14 12:59:00 +1000

    Modified QSettings wrapper to use a metaclass and properties rather than the difficult to debug __getattr__ and __setattr__ methods.

--- qsettings_wrapper.py
+++ qsettings_wrapper.py
@@ -1,26 +1,29 @@
 from PySide.QtCore import QSettings
 import ast
 
+class type_with_properties(type):
+    """A metaclass to create properties for a class based on the contents
+    of its a class _fields. _fields should be a list of strings. At
+    class creation, property getters and setters will be created,
+    hooking each name up to _get and _set class methods."""
+    def __init__(cls, name, superclasses, attrs):
+        type.__init__(cls, name, superclasses, attrs)
+        for name in cls._fields:
+            method = property(lambda self, name=name: cls._get(self, name),
+                              lambda self, value, name=name: cls._set(self, name, value))
+            setattr(cls, name, method)
+    
 class QSettingsWrapper(object):
     """A class that wraps QSettings to provide automatic type conversion
     and reduce the need for boilerplace code"""
-    def __init__(self, companyname, appname, fields):
+    __metaclass__ = type_with_properties
+    _fields = [] # to be overridden by subclasses
+    def __init__(self, companyname, appname):
         self._qsettings = QSettings(companyname, appname)
-        self._fields = fields
+        # set default values to None:
         if not self._qsettings.contains(self._fields[0]):
-            # set default values to None:
             for name in self._fields:
                 self._set(name, None)
-        
-    def __getattr__(self, name):
-        if name in self._fields:
-            return self._get(name)
-        raise AttributeError
-            
-    def __setattr__(self, name, value):
-        if hasattr(self, '_fields') and name in self._fields:
-            self._set(name, value)
-        object.__setattr__(self, name, value)
         
     def _get(self, name):
         valrepr = self._qsettings.value(name)
@@ -35,4 +38,19 @@ class QSettingsWrapper(object):
         self._qsettings.setValue(name, valrepr)
         self._qsettings.sync()
         
+        
+if __name__ == '__main__':
+    
+    # Test and example implementation
+    
+    class TestSettings(QSettingsWrapper):
+        _fields = ['test']
+        def __init__(self):
+            QSettingsWrapper.__init__(self, 'test', 'test')
+            
+    s = TestSettings()
+    s.test = 5
+    assert s.test == 5
+    s.test += 2
+    assert s.test == 7
     
