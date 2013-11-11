83098c6638abf58f7305db6a780e3f2ff6cf8c08
Phil Starkey <philipstarkey@users.noreply.github.com>
2013-11-12 10:27:42 +1100

    Modified invoke_in_main to not "leak" memory
    I put "leak" in quotes because it is yet unclear whether it actually does leak memory. What we do know is that the memory usage of a program using the in_main function does grow, but periodically it drops significantly. Longer testing must be done before we can tell whether we actually have a memory leak.
    
    However, I have discovered that the Qt documentation states that an event passed to QCoreApplication.postEvent() should not be accessed after it is posted. We were accessing it to wait for the event to be processed. When we don't access it after calling QCoreApplication.postEvent() (and instead pass in a queue at event instantiation, which we then wait on), the memory usage does not grow nearly as rapidly and from preliminary observations, oscialltes around an average usage.
    
    Further testing will ensue.

--- invoke_in_main.py
+++ invoke_in_main.py
@@ -2,109 +2,86 @@ import sys
 import Queue
 import threading
 import functools
-import inspect
 
 from PySide.QtCore import *
-
-from locking import qtlock
 
 class CallEvent(QEvent):
     """An event containing a request for a function call."""
     EVENT_TYPE = QEvent.Type(QEvent.registerEventType())
-    def __init__(self, fn, *args, **kwargs):
+    def __init__(self, queue, fn, *args, **kwargs):
         QEvent.__init__(self, self.EVENT_TYPE)
         self.fn = fn
         self.args = args
         self.kwargs = kwargs
-        self.done = False
-        self._returnval = Queue.Queue()
-        self._lock = threading.Lock()
-        self._exc_info = None
-        self.cancelled = False
+        self._returnval = queue
         # Whether to raise exceptions in the main thread or store them
         # for raising in the calling thread:
         self._exceptions_in_main = True
-        
-    def wait(self):
-        if qtlock.held():
-            message = ('Deadlock: Either you are already in the main thread or you have acquired the qtlock. ' +
-                       'Either way the qt mainloop is blocked and the function you are waiting on will never run')
-            raise threading.ThreadError(message)
-        # Any exceptions will now be raised here, we don't need to raise
-        # them in the main thread too. If the exception has already
-        # been raised in the main thread by this point, that's ok,
-        # we'll raise it here as well.
-        self._exceptions_in_main = False
-        result = self._returnval.get()
-        if self._exc_info is not None:
-            # If there was an exception, raise it:
-            type, value, traceback = self._exc_info
-            raise type, value, traceback
-        return result
     
-    def cancel(self):
-        with self._lock:
-            if self.done:
-                return False
-            self.cancelled = True
-            return True
-                
-                
+
 class Caller(QObject):
     """An event handler which calls the function held within a CallEvent."""
     def event(self, event):
-        with event._lock:
-            if not event.cancelled:
-                try:
-                    result = event.fn(*event.args, **event.kwargs)
-                except Exception:
-                    # Store for re-raising the exception in the calling thread:
-                    event._exc_info = sys.exc_info()
-                    result = None
-                    if event._exceptions_in_main:
-                        # Or, if nobody is listening for this exception,
-                        # better raise it here so it doesn't pass
-                        # silently:
-                        raise
-                finally:
-                    event._returnval.put(result)
-                    event.done = True
+        event.accept()
+        exception = None
+        try:
+            result = event.fn(*event.args, **event.kwargs)
+        except Exception:
+            # Store for re-raising the exception in the calling thread:
+            exception = sys.exc_info()
+            result = None
+            if event._exceptions_in_main:
+                # Or, if nobody is listening for this exception,
+                # better raise it here so it doesn't pass
+                # silently:
+                raise
+        finally:
+            event._returnval.put([result,exception])
         return True
         
 caller = Caller()
 
-
 def inmain(fn, *args, **kwargs):
     """Execute a function in the main thread. Wait for it to complete
     and return its return value."""
-    if threading.current_thread().name == 'MainThread':
-        return fn(*args, **kwargs)
-    event = CallEvent(fn, *args, **kwargs)
-    event._exceptions_in_main = False
-    QCoreApplication.postEvent(caller, event)
-    return event.wait()
+    return get_inmain_result(in_main_later(fn,False,*args,**kwargs)
     
-def inmain_later(fn, *args, **kwargs):
+def in_main_later(self, fn, exceptions_in_main, *args, **kwargs):
     """Asks the mainloop to call a function when it has time. Immediately
-    returns the event that was sent to the mainloop.  Functions are
-    guaranteed to be called in the order they were requested."""
-    event = CallEvent(fn, *args, **kwargs)
+    returns the queue that was sent to the mainloop.  A call to queue.get()
+    will return a list of [result,exception] where exception=[type,value,traceback]
+    of the exception.  Functions are guaranteed to be called in the order
+    they were requested."""        
+    queue = Queue.Queue()
+    event = CallEvent(queue, fn, *args, **kwargs)
+    event._exceptions_in_main = exceptions_in_main
     QCoreApplication.postEvent(caller, event)
-    return event
+    return queue
+    
+def get_inmain_result(self,queue):
+    result,exception = queue.get()
+    if exception is not None:
+        type, value, traceback = exception
+        raise type, value, traceback
+    return result
 
 def inthread(f,*args,**kwargs):
     thread = threading.Thread(target=f, args=args, kwargs=kwargs)
     thread.daemon=True
     thread.start()
+    return thread
     
-def inmain_decorator(wait_for_return=True):
+def inmain_decorator(wait_for_return=True,exceptions_in_main=True):
+    """ A decorator which sets any function t always run in the main thread
+    If wait_for_return=True, then exceptions_in_main is ignored.
+    """
     def wrap(fn):
         """A decorator which sets any function to always run in the main thread."""
         @functools.wraps(fn)
         def f(*args,**kwargs):
             if wait_for_return:
                 return inmain(fn, *args, **kwargs)
-            return inmain_later(fn, *args, **kwargs)  
+            return inmain_later(fn, exceptions_in_main, *args, **kwargs)  
         return f
     return wrap
     
