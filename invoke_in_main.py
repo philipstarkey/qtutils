9afefbb2099b5a93cdc1e305b0109eecf9770eb5
pstarkey <pstarkey@localhost>
2013-12-09 14:14:44 +1100

    Worked around memory leak caused by PostEvent
    inmain, inmain_later and the inmain_decorator can now only be used from the Main Thread or an instance of the InMainThread (based on a QThread)

--- invoke_in_main.py
+++ invoke_in_main.py
@@ -41,6 +41,11 @@ class Caller(QObject):
          
 caller = Caller()
 
+def post_event(tup):
+    queue, exceptions_in_main, fn, args, kwargs = tup
+    event = CallEvent(queue, exceptions_in_main, fn, *args, **kwargs)
+    QCoreApplication.postEvent(caller,event)
+
 def inmain(fn, *args, **kwargs):
     """Execute a function in the main thread. Wait for it to complete
     and return its return value."""
@@ -58,9 +63,13 @@ def in_main_later(fn, exceptions_in_main, *args, **kwargs):
     of the exception.  Functions are guaranteed to be called in the order
     they were requested."""        
     queue = Queue.Queue()
-    QCoreApplication.postEvent(caller, CallEvent(queue, exceptions_in_main, fn, *args, **kwargs))
+    try:
+        qthread = QThread.currentThread()
+        qthread.postingEvent.emit((queue, exceptions_in_main, fn, args, kwargs))
+    except Exception:
+        raise RuntimeError('You can only call the inmain and inmain_later functions, or a function decorated with the inmain_decorator, from an InMainThread or the main thread.')
     return queue
-    
+  
 def get_inmain_result(queue):
     result,exception = queue.get()
     if exception is not None:
@@ -68,11 +77,6 @@ def get_inmain_result(queue):
         raise type, value, traceback
     return result
 
-def inthread(f,*args,**kwargs):
-    thread = threading.Thread(target=f, args=args, kwargs=kwargs)
-    thread.daemon=True
-    thread.start()
-    return thread
     
 def inmain_decorator(wait_for_return=True,exceptions_in_main=True):
     """ A decorator which sets any function to always run in the main thread
@@ -88,3 +92,93 @@ def inmain_decorator(wait_for_return=True,exceptions_in_main=True):
         return f
     return wrap
     
+    
+class InMainThread(QThread):
+    postingEvent = Signal(tuple)
+    def __init__(self, target, parent = None, args = (), kwargs = {}):
+        """
+        Must be instantiated in the main thread or the target function of another
+        instance of this class
+        
+        An exception will be raised if instantiated from a plain Python thread or 
+        a standard QThread.
+        """
+        # runs in the thread in which the thread is instantiated        
+        QThread.__init__(self,parent)
+        self.func = target
+        self.args = args
+        self.kwargs = kwargs
+        # If created in the main thread, this is just run in the main thread
+        # If created in another instance of this class, then inmain will
+        #    use that threads postingEvent signal to run the code in the main thread
+        inmain(self.postingEvent.connect,post_event)
+
+###
+# Deamon threads code commented out because I can't seem to make a NON-daemon QThread
+###
+        # # configure default value of daemon property
+        # self._daemon_thread = False        
+        # try:
+            # qthread = QThread.currentThread()
+            # if isinstance(qthread,InMainThread):
+                # self.daemon = qthread.daemon
+        # except Exception:
+            # pass
+        
+    # @property
+    # def daemon(self):
+        # return self._daemon_thread
+    
+    # # Daemon property. If a thread is a daemon thread, it should be force terminated when the 
+    # # eventloop in the main thread exits 
+    # @daemon.setter
+    # def daemon(self,value):
+        # if self.isRunning():
+            # raise RuntimeError('You cannot change the daemon property of this thread after it has been started')
+        # value = bool(value)
+        # self._daemon_thread = value
+        # if self.daemon:
+            # print 'setting daemon True'
+            # # connect finished signal
+            # self.finished.connect(self.onFinished)
+            # # Connect about to Quit signal
+            # QCoreApplication.instance().aboutToQuit.connect(self.terminate)
+    
+    # def onFinished(self):
+        # print 'thread finished'
+        # QCoreApplication.instance().aboutToQuit.disconnect(self.terminate)
+        
+    def run(self):
+        self.func(*self.args,**self.kwargs)
+    
+if __name__ == '__main__':   
+    import signal
+    
+    def loop(index):
+        if index < 3:
+            thread = InMainThread(target=loop,args=(index+1,))
+            thread.start()
+        while True:
+            # print 'MyThread-%d: %s'%(index,str(QThread.currentThread()))
+            # print 'MyThread-%d: %s'%(index,threading.current_thread().name)
+            another_function(index)
+            inmain(myFunction,index)
+    
+    def another_function(index):
+        print 'in thread-%d, running in thread: %s'%(index,threading.currentThread().name)
+    
+    def myFunction(index):
+        print 'from thread-%d, running in thread: %s'%(index,threading.currentThread().name)
+        pass
+
+    qapplication = QCoreApplication(sys.argv)
+    def sigint_handler(*args):
+        qapplication.quit()
+      
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    thread = InMainThread(target=loop, args=(1,))
+    thread.start() 
+
+    qapplication.exec_()
+
