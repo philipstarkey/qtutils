3449b4813426f6e183f1e11fca3dcc6b45c0950a
Phil Starkey <philipstarkey@users.noreply.github.com>
2014-01-23 13:46:04 +1100

    Added license to this project. Please note that since this repository was private up until this point, all previous versions of the code in the version control history, are also licensed under the 2-clause BSD license.

--- locking.py
+++ locking.py
@@ -1,3 +1,17 @@
+#####################################################################
+#                                                                   #
+# locking.py                                                        #
+#                                                                   #
+# Copyright 2013, Christopher Billington, Philip Starkey            #
+#                                                                   #
+# This file is part of the qtutils project                          #
+# (see https://bitbucket.org/philipstarkey/qtutils )                #
+# and is licensed under the Simplified BSD License.                 #
+# See the license.txt file in the root of the project               #
+# for the full license.                                             #
+#                                                                   #
+#####################################################################
+
 import threading
 from PySide.QtCore import *
 
