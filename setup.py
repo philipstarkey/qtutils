df302377cce9dca324b4df31f3fcba4a6685716d
Chris Billington <chrisjbillington@gmail.com>
2014-08-19 17:37:33 -0400

    Added the Fugue icon set, which is CC licensed and free to use with attribution. Importing qtutils.icons will build the required resource file icons.qrc and python resource module _icons.py. The latter is then automatically imported, adding the resources to any Qt application code importing this module. This code assumes PyQt4 and will not work with PySide.
    To use for development:
    
    import qtutils.icons once, from anywhere, to do the build process.
    
    Point Qt Designer to <qtutils directory>/icons/icons.qrc. You will then be able
    to select icons from within Qt Designer.
    
    From your application, import qtutils.icons before loading the GUI. This will
    register the resources with PyQt4 by importing the autogenerated _icons.py.
    
    I still need to sort out distribution issues - need to do the build at install
    time rather than runtime, and ensure devs have access to the .qrc file, which
    Qt Designer won't be able to see if it is stuck inside an .egg file. Eggs are
    going the way of the dinosaur though, so perhaps this won't be a problem.
    Installing from pip should be fine. I will investigate.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from distutils.core import setup
 import os
 
-VERSION = '1.2.1'
+VERSION = '1.3.0-dev'
 
 # Auto generate a __version__ package for the package to import
 with open(os.path.join('qtutils', '__version__.py'), 'w') as f:
@@ -19,10 +19,10 @@ with open(os.path.join('qtutils', '__version__.py'), 'w') as f:
     
 setup(name='qtutils',
       version=VERSION,
-      description='Utilities for providing concurrent access to Qt objects, simplified QSettings storage, and dynamic widget promotion when loading UI files, in Python Qt applications.',
+      description='Utilities for providing concurrent access to Qt objects, simplified QSettings storage, and dynamic widget promotion when loading UI files, in Python Qt applications. Also includes the Fugue icon set, by Yusuke Kamiyamane',
       author='Philip Starkey',
       author_email='threepineapples@gmail.com',
       url='https://bitbucket.org/philipstarkey/qtutils',
       license="2-clause BSD, 3-clause BSD (see LICENSE.TXT for full conditions)",
-      packages=['qtutils'],
+      packages=['qtutils', 'qtutils.icons'],
      )
