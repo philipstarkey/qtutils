4a6d2ccb1cef0a7d498adb335cb548279f486c32
Jan Werkmann <26031495+PhyNerd@users.noreply.github.com>
2017-07-22 17:54:00 +0200

    removed unused widget_names and reordered monkeypatches

--- qtutils/Qt.py
+++ qtutils/Qt.py
@@ -21,9 +21,6 @@ PYSIDE = 'PySide'
 PYQT4 = 'PyQt4'
 PYQT5 = 'PyQt5'
 
-widget_names = ['QAbstractButton', 'QGraphicsAnchor', 'QMacCocoaViewContainer', 'QStyleOptionGroupBox', 'QAbstractGraphicsShapeItem', 'QGraphicsAnchorLayout', 'QMainWindow', 'QStyleOptionHeader', 'QAbstractItemDelegate', 'QGraphicsBlurEffect', 'QMdiArea', 'QStyleOptionMenuItem', 'QAbstractItemView', 'QGraphicsColorizeEffect', 'QMdiSubWindow', 'QStyleOptionProgressBar', 'QAbstractScrollArea', 'QGraphicsDropShadowEffect', 'QMenu', 'QStyleOptionRubberBand', 'QAbstractSlider', 'QGraphicsEffect', 'QMenuBar', 'QStyleOptionSizeGrip', 'QAbstractSpinBox', 'QGraphicsEllipseItem', 'QMessageBox', 'QStyleOptionSlider', 'QAction', 'QGraphicsGridLayout', 'QMouseEventTransition', 'QStyleOptionSpinBox', 'QActionGroup', 'QGraphicsItem', 'QOpenGLWidget', 'QStyleOptionTab', 'QApplication', 'GraphicsItemFlags', 'OptimizationFlags', 'QStyleOptionTabBarBase', 'AreaOptions', 'QGraphicsItemGroup', 'Options', 'QStyleOptionTabWidgetFrame', 'AutoFormatting', 'QGraphicsLayout', 'Options', 'QStyleOptionTitleBar', 'BlurHints', 'QGraphicsLayoutItem', 'QPanGesture', 'QStyleOptionToolBar', 'QBoxLayout', 'QGraphicsLineItem', 'QPinchGesture', 'QStyleOptionToolBox', 'ButtonFeatures', 'QGraphicsLinearLayout', 'QPlainTextDocumentLayout', 'QStyleOptionToolButton', 'QButtonGroup', 'QGraphicsObject', 'QPlainTextEdit', 'QStyleOptionViewItem', 'CacheMode', 'QGraphicsOpacityEffect', 'QProgressBar', 'QStylePainter', 'QCalendarWidget', 'QGraphicsPathItem', 'QProgressDialog', 'QStyledItemDelegate', 'ChangeFlags', 'QGraphicsPixmapItem', 'QProxyStyle', 'SubControls', 'ChangeFlags', 'QGraphicsPolygonItem', 'QPushButton', 'SubWindowOptions', 'QCheckBox', 'QGraphicsProxyWidget', 'QRadioButton', 'QSwipeGesture', 'QColorDialog', 'QGraphicsRectItem', 'RenderFlags', 'QSystemTrayIcon', 'ColorDialogOptions', 'QGraphicsRotation', 'Result', 'QTabBar', 'QColumnView', 'QGraphicsScale', 'QRubberBand', 'TabFeatures', 'QComboBox', 'QGraphicsScene', 'SceneLayers', 'QTabWidget', 'QCommandLinkButton', 'QGraphicsSceneContextMenuEvent', 'QScrollArea', 'QTableView', 'QCommonStyle', 'QGraphicsSceneDragDropEvent', 'QScrollBar', 'QTableWidget', 'QCompleter', 'QGraphicsSceneEvent', 'QScroller', 'QTableWidgetItem', 'ControlTypes', 'QGraphicsSceneHelpEvent', 'QScrollerProperties', 'QTableWidgetSelectionRange', 'CornerWidgets', 'QGraphicsSceneHoverEvent', 'Sections', 'TakeRowResult', 'QDataWidgetMapper', 'QGraphicsSceneMouseEvent', 'QShortcut', 'QTapAndHoldGesture', 'QDateEdit', 'QGraphicsSceneMoveEvent', 'QSizeGrip', 'QTapGesture', 'QDateTimeEdit', 'QGraphicsSceneResizeEvent', 'QSizePolicy', 'QTextBrowser', 'QDesktopWidget', 'QGraphicsSceneWheelEvent', 'QSlider', 'QTextEdit', 'QDial', 'QGraphicsSimpleTextItem', 'QSpacerItem', 'QTimeEdit', 'QDialog', 'QGraphicsTextItem', 'QSpinBox', 'QToolBar', 'QDialogButtonBox', 'QGraphicsTransform', 'QSplashScreen', 'ToolBarFeatures', 'QDirModel', 'QGraphicsView', 'QSplitter', 'QToolBox', 'DockOptions', 'QGraphicsWidget', 'QSplitterHandle', 'QToolButton', 'QDockWidget', 'QGridLayout', 'QStackedLayout', 'ToolButtonFeatures', 'DockWidgetFeatures', 'QGroupBox', 'QStackedWidget', 'QToolTip', 'QDoubleSpinBox', 'QHBoxLayout', 'StandardButtons', 'QTreeView', 'EditTriggers', 'QHeaderView', 'StandardButtons', 'QTreeWidget', 'QErrorMessage', 'QInputDialog', 'State', 'QTreeWidgetItem', 'ExtraSelection', 'InputDialogOptions', 'QStatusBar', 'QTreeWidgetItemIterator', 'QFileDialog', 'QItemDelegate', 'StepEnabled', 'QUndoCommand', 'QFileIconProvider', 'QItemEditorCreatorBase', 'QStyle', 'QUndoGroup', 'QFileSystemModel', 'QItemEditorFactory', 'QStyleFactory', 'QUndoStack', 'QFocusFrame', 'IteratorFlags', 'QStyleHintReturn', 'QUndoView', 'QFontComboBox', 'QKeyEventTransition', 'QStyleHintReturnMask', 'QVBoxLayout', 'QFontDialog', 'QKeySequenceEdit', 'QStyleHintReturnVariant', 'ViewItemFeatures', 'FontDialogOptions', 'QLCDNumber', 'QStyleOption', 'QWhatsThis', 'FontFilters', 'QLabel', 'QStyleOptionButton', 'QWidget', 'QFormLayout', 'QLayout', 'QStyleOptionComboBox', 'QWidgetAction', 'QFrame', 'QLayoutItem', 'QStyleOptionComplex', 'QWidgetItem', 'FrameFeatures', 'QLineEdit', 'QStyleOptionDockWidget', 'QWizard', 'QGesture', 'QListView', 'QStyleOptionFocusRect', 'WizardOptions', 'QGestureEvent', 'QListWidget', 'QStyleOptionFrame', 'QWizardPage', 'QGestureRecognizer', 'QListWidgetItem', 'QStyleOptionGraphicsItem']
-
-
 libs = [PYQT5, PYQT4, PYSIDE]
 for lib in libs:
     try:
@@ -47,11 +44,8 @@ else:
         import PySide
         QtCore.QT_VERSION_STR = PySide.QtCore.__version__
         QtCore.PYQT_VERSION_STR = PySide.__version__
-    QtWidgets = QtGui
-    QtCore.QSortFilterProxyModel = QtGui.QSortFilterProxyModel
-    QtWidgets.QStyleOptionProgressBar = QtGui.QStyleOptionProgressBarV2
 
-    class NewQHeaderView(QtWidgets.QHeaderView):
+    class NewQHeaderView(QtGui.QHeaderView):
         def setSectionsMovable(self, *args, **kwargs):
             self.setMovable(*args, **kwargs)
 
@@ -61,14 +55,17 @@ else:
         def setSectionResizeMode(self, *args, **kwargs):
             self.setResizeMode(*args, **kwargs)
 
-    QtWidgets.QHeaderView = NewQHeaderView
+    QtGui.QHeaderView = NewQHeaderView
 
-    class NewQFileDialog(QtWidgets.QFileDialog):
+    class NewQFileDialog(QtGui.QFileDialog):
         def getOpenFileName(self, *args, **kwargs):
             self.getOpenFileNamesAndFilter(*args, **kwargs)
 
-    QtWidgets.QFileDialog = NewQFileDialog
+    QtGui.QFileDialog = NewQFileDialog
 
+    QtWidgets = QtGui
+    QtCore.QSortFilterProxyModel = QtGui.QSortFilterProxyModel
+    QtWidgets.QStyleOptionProgressBar = QtGui.QStyleOptionProgressBarV2
 
 sys.modules['qtutils.Qt.QtGui'] = QtGui
 sys.modules['qtutils.Qt.QtWidgets'] = QtWidgets
