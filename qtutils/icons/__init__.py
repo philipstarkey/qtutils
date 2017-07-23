b938f6f141525efe34738559d6b5e9a3d1c76a4c
Jan Werkmann <26031495+PhyNerd@users.noreply.github.com>
2017-07-23 02:42:21 +0200

    replaced is with == and fixed renaming error

--- qtutils/icons/__init__.py
+++ qtutils/icons/__init__.py
@@ -1,7 +1,7 @@
 import qtutils.qt
-if qtutils.qt.QT_ENV is 'Pyside':
+if qtutils.qt.QT_ENV == 'Pyside':
     import qtutils.icons._icons_pyside
-elif qtutils.qt.QT_ENV is 'PyQt4':
-        import qtutils.icons._icons_pyqt4
-elif qtutils.qt.QT_ENV is 'PyQt5':
+elif qtutils.qt.QT_ENV == 'PyQt4':
+    import qtutils.icons._icons_pyqt4
+elif qtutils.qt.QT_ENV == 'PyQt5':
     import qtutils.icons._icons_pyqt5
