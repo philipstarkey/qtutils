ea88158dbacdc4e2eb3f42f6f220b7ce009cf9cc
PhyNerd <janwerkmann@gmail.com>
2017-08-01 20:22:28 +0200

    Changed Qt environment strings to constanst

--- qtutils/icons/__init__.py
+++ qtutils/icons/__init__.py
@@ -1,7 +1,7 @@
 import qtutils.qt
-if qtutils.qt.QT_ENV == 'Pyside':
+if qtutils.qt.QT_ENV == qtutils.qt.PYSIDE:
     import qtutils.icons._icons_pyside
-elif qtutils.qt.QT_ENV == 'PyQt4':
+elif qtutils.qt.QT_ENV == qtutils.qt.PYQT4:
     import qtutils.icons._icons_pyqt4
-elif qtutils.qt.QT_ENV == 'PyQt5':
+elif qtutils.qt.QT_ENV == qtutils.qt.PYQT5:
     import qtutils.icons._icons_pyqt5
