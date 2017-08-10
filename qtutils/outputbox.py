0f88034909326db027180431fd42d2a7117b4e20
Chris Billington <chrisjbillington@gmail.com>
2017-08-10 00:39:23 -0400

    separate print() and write() methods for the output box, one of which obeys the semantics of thr python print() function (newlines, separators), and the other of which is more like file.write(), with no newlines added or anything.
    Added colour, bold and italics options for text added with these methods
    (or text received over the socket).
    
    The old output(text, red=False) method is retained for backward compatibility.

--- qtutils/outputbox.py
+++ qtutils/outputbox.py
@@ -22,6 +22,7 @@ from qtutils.qt.QtWidgets import *
 import zmq
 from qtutils.auto_scroll_to_end import set_auto_scroll_to_end
 from qtutils import *
+import ast
 
 # This should cover most platforms:
 acceptable_fonts = ["Ubuntu mono",
@@ -35,6 +36,48 @@ acceptable_fonts = ["Ubuntu mono",
                     "monospace"]
 
 
+GREY = GRAY = '#75715E' 
+BACKGROUND = '#141411'
+WHITE = '#FFFFFF'
+RED = '#EF2020'
+ORANGE = '#FD971F'
+YELLOW = '#FCE94F'
+GREEN = '#A6E22E'
+BLUE = '#66D9EF'
+PURPLE = '#AE81FF'
+
+
+_charformats = {}
+
+def charformats(charformat_repr):
+    try:
+        return _charformats[charformat_repr]
+    except KeyError:
+        pass
+    try:
+        color, bold, italic = ast.literal_eval(charformat_repr)
+    except Exception:
+        if charformat_repr == 'stderr':
+            color, bold, italic = 'red', False, False
+        else:
+            # stdout, or invalid spec. Use plain font:
+            color, bold, italic = WHITE, False, False
+    try:
+        qcolor = QColor(color)
+    except Exception:
+        # invalid color, use white:
+        qcolor = QColor(WHITE)
+    font = QFont("SomeMonoFont", 11)
+    font.insertSubstitutions("SomeMonoFont", acceptable_fonts)
+    font.setBold(bold)
+    font.setItalic(italic)
+    fmt = QTextCharFormat()
+    fmt.setForeground(QBrush(qcolor))
+    fmt.setFont(font)
+    _charformats[color, bold, italic] = fmt
+    return fmt
+
+
 class OutputBox(object):
 
     def __init__(self, container, scrollback_lines=1000):
@@ -42,7 +85,7 @@ class OutputBox(object):
         container.addWidget(self.output_textedit)
         self.output_textedit.setReadOnly(True)
         palette = self.output_textedit.palette()
-        palette.setColor(QPalette.Base, QColor('black'))
+        palette.setColor(QPalette.Base, QColor(BACKGROUND))
         self.output_textedit.setPalette(palette)
 
         self.output_textedit.setBackgroundVisible(False)
@@ -53,19 +96,6 @@ class OutputBox(object):
         # Keeping track of whether the output is in the middle of a line
         # or not:
         self.mid_line = False
-
-        normal_font = QFont("SomeMonoFont", 11)
-        normal_font.insertSubstitutions("SomeMonoFont", acceptable_fonts)
-        self.normal_text_format = QTextCharFormat()
-        self.normal_text_format.setForeground(QBrush(QColor('white')))
-        self.normal_text_format.setFont(normal_font)
-
-        red_font = QFont("SomeMonoFont", 11)
-        red_font.insertSubstitutions("SomeMonoFont", acceptable_fonts)
-        red_font.setBold(True)
-        self.red_text_format = QTextCharFormat()
-        self.red_text_format.setForeground(QBrush(QColor('red')))
-        self.red_text_format.setFont(red_font)
 
         context = zmq.Context.instance()
         socket = context.socket(zmq.PULL)
@@ -97,16 +127,66 @@ class OutputBox(object):
         self.local.push_sock = context.socket(zmq.PUSH)
         self.local.push_sock.connect('tcp://127.0.0.1:%d' % self.port)
 
-    def output(self, text, red=False):
+    def write(self, text, color=WHITE, bold=False, italic=False):
+        """Write to the output box as if it were a file. Takes a string as
+        does not append newlines or anything else. use OutputBox.print() for
+        an interface more like the Python print() function."""
         if not hasattr(self.local, 'push_sock'):
             self.new_socket()
         # Queue the output on the socket:
-        self.local.push_sock.send_multipart(['stderr' if red else 'stdout', text.encode('utf8')])
+        charformat = repr((color, bold, italic)).encode('utf8')
+        self.local.push_sock.send_multipart([charformat, text.encode('utf8')])
+
+    def print(self, *values, **kwargs):
+        """Print to the output box. This method accepts the same arguments as
+        the Python print function. If file=sys.stderr, the output will be red
+        and bold. If it is absent or sys.stdout, it will be white. Anything
+        else is an exception. The 'color' and 'bold' keyword arguments if
+        provided will override the settings inferred from the file keyword
+        argument."""
+
+        sep = kwargs.pop('sep', None)
+        end = kwargs.pop('end', None)
+        file = kwargs.pop('file', None)
+
+        if sep is None:
+            sep = ' '
+        if end is None:
+            end = '\n'
+        if file is None:
+            file = sys.stdout
+
+        
+
+        if file is sys.stdout:
+            color = WHITE
+            bold = False
+        elif file is sys.stderr:
+            color = 'red'
+            bold = False
+        else:
+            msg = 'file argument for OutputBox.print() must be stdout or stderr'
+            raise ValueError(msg)
+        bold = kwargs.pop('bold', bold)
+        color = kwargs.pop('color', color)
+        italic = kwargs.pop('italic', False)
+        self.write(sep.join(str(s) for s in values) + end, color=color, bold=bold, italic=italic)
+
+    def output(self, text, red=False):
+        """Wrapper around write() with only option for normal text or bold red
+        text, retained for backward compatibility but deprecated."""
+        if red:
+            color = 'red'
+            bold = False
+        else:
+            color = WHITE
+            bold = False
+        self.write(text, color=color, bold=bold)
 
     def mainloop(self, socket):
         while True:
             messages = []
-            current_stream = None
+            current_charformat = None
             # Wait for messages
             socket.poll()
             # Get all messages waiting in the pipe, concatenate strings to
@@ -114,21 +194,20 @@ class OutputBox(object):
             # to the qt main thread, which can be a bottleneck when there is a lot of output)
             while True:
                 try:
-                    stream, text = socket.recv_multipart(zmq.NOBLOCK)
+                    charformat_repr, text = socket.recv_multipart(zmq.NOBLOCK)
                 except zmq.Again:
                     break
-                if stream != current_stream:
-                    current_stream = stream
+                if charformat_repr != current_charformat:
+                    current_charformat = charformat_repr
                     current_message = []
-                    messages.append((current_stream, current_message))
+                    messages.append((current_charformat, current_message))
                 current_message.append(text)
-            for stream, message in messages:
-                text = ''.join(message).decode('utf8')
-                red = (stream == 'stderr')
-                self.add_text(text, red)
+            for charformat_repr, message in messages:
+                text = b''.join(message).decode('utf8')
+                self.add_text(text, charformat_repr.decode('utf8'))
 
     @inmain_decorator(True)
-    def add_text(self, text, red):
+    def add_text(self, text, charformat_repr):
         # The convoluted logic below is because we want a few things that
         # conflict slightly. Firstly, we want to take advantage of our
         # setMaximumBlockCount setting; Qt will automatically remove old
@@ -162,10 +241,7 @@ class OutputBox(object):
         cursor.movePosition(QTextCursor.End)
         cursor.movePosition(QTextCursor.PreviousCharacter, n=n_chars_printed)
         cursor.movePosition(QTextCursor.End, mode=QTextCursor.KeepAnchor)
-        if red:
-            cursor.setCharFormat(self.red_text_format)
-        else:
-            cursor.setCharFormat(self.normal_text_format)
+        cursor.setCharFormat(charformats(charformat_repr))
 
 
 if __name__ == '__main__':
@@ -175,14 +251,40 @@ if __name__ == '__main__':
     layout = QVBoxLayout(window)
 
     output_box = OutputBox(layout)
+
     for i in range(3):
-        output_box.output('white, two line breaks.\n\n')
-        output_box.output('white, no linebreak.')
-        output_box.output('Red.\n', True)
-        output_box.output('More red.\n', True)
-        output_box.output('The \"quick white fox\" jumped over the \'lazy\' dog\n')
-        output_box.output('<The quick red fox jumped over the lazy dog>\n', True)
-        output_box.output('Der schnelle braune Fuchs ist \xc3\xbcber den faulen Hund gesprungen\n'.decode('utf8'), True)
+        output_box.write('white, two line breaks.\n\n')
+        output_box.write('white, no linebreak.')
+        output_box.write('Red.\n', color='red')
+        output_box.output('Red with the deprecated function.\n', red=True)
+        output_box.write('More red, also bold.\n', color='red', bold=True)
+        output_box.write('The \"quick white fox\" jumped over the \'lazy\' dog\n')
+        output_box.write('<The bold, green, italic fox jumped over the lazy dog>\n', color=GREEN, bold=True, italic=True)
+        output_box.write(b'Der schnelle braune Fuchs ist \xc3\xbcber den faulen Hund gesprungen\n'.decode('utf8'))
+
+        output_box.print("print test")
+        output_box.print("stderr", "test", 123, file=sys.stderr)
+
+        output_box.print()
+
+    for i in range(3):
+        output_box.print('This is just some info from runmanager in bold blue', bold=True, italic=True, color=BLUE)
+        output_box.print('Some more info in a subdued grey', italic=True, color=GREY)
+
+    for i in range(2):
+        output_box.print('\nthis is runmanager running your script in purple', color=PURPLE)
+        for j in range(5):
+            output_box.print('The \"quick white fox\" jumped over the \'lazy\' dog')
+        if i:
+            output_box.print("RunTimeError('your script broke')", file=sys.stderr)
+            output_box.print('it failed in bold red', color=RED, bold=True, italic=True)
+        else:
+            output_box.print("Your script worked fine")
+            output_box.print('it worked in green', color=GREEN, italic=True)
+            output_box.print('submitting to BLACS in yellow...', color=YELLOW, italic=True, end='')
+            output_box.print('success in bold green', color=GREEN, bold=True, italic=True)
+            output_box.print('warning: queue is paused in orange', color=ORANGE, italic=True)
+            
 
     def button_pushed(*args, **kwargs):
         import random
@@ -192,7 +294,11 @@ if __name__ == '__main__':
             ustr += unichr(uc)
         red = random.randint(0, 1)
         newline = random.randint(0, 1)
-        output_box.output(ustr + ('\n' if newline else ''), red=red)
+        hex = __builtins__.hex
+        color = '#' + hex(random.randint(0, 0xffffff))[2:]
+        bold = random.randint(0, 1)
+        italic = random.randint(0, 1)
+        output_box.write(ustr + ('\n' if newline else ''), color=color, bold=bold, italic=italic)
 
     button = QPushButton("push me to output random text")
     button.clicked.connect(button_pushed)
