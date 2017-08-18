ea611f1ac412e4899cf759f5f6a9610becd17078
Jan Werkmann <26031495+PhyNerd@users.noreply.github.com>
2017-08-18 18:57:28 +0200

    removed more redunant print_function imports and explicitly casted string to bytestring for ModleType in python2

--- qtutils/icons/__init__.py
+++ qtutils/icons/__init__.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from __future__ import division, unicode_literals, print_function, absolute_import
 import qtutils.qt
 if qtutils.qt.QT_ENV == qtutils.qt.PYSIDE:
--- qtutils/icons/_build.py
+++ qtutils/icons/_build.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from __future__ import division, unicode_literals, print_function, absolute_import
 
 import os
