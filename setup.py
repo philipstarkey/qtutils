f93a5fb8f7264eb0e3e96a289600968f974b47bf
Chris Billington <chrisjbillington@gmail.com>
2018-07-19 14:40:54 -0400

    Bump version for release.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if 'NO_PYQT5' in sys.argv:
     BUILD_PYQT5_ICONS_RESOURCE = False
     sys.argv.remove('NO_PYQT5')
 
-VERSION = '2.1.0'
+VERSION = '2.2.0'
 
 # conditional for readthedocs environment
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
